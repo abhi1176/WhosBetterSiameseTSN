0f22e685de57abf7be5cb6dd47672d4955e54090
Abhishek Korra <akorra@xsjfislx20.xilinx.com>
2021-04-03 15:58:56 -0700

    prepare_data.py: Update

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ mkdir dataset && cd dataset
 
 # [ChopstickUsing, HandDrawing and SonicDrawing]
 # Download https://drive.google.com/file/d/1Ck-Dke5AcKMzKsedJfblRcTjSPBklo4P/view to `dataset`
-sudo unzip EPIC-Skills2018_videos.zip  # This didn't work on Linux, but did on Windows. Don't know why
+unzip EPIC-Skills2018_videos.zip  # This didn't work on Linux, but did on Windows. Don't know why
 cd EPIC-Skills2018_videos/videos
 for dir in `ls`
 do
@@ -24,8 +24,8 @@ mkdir DoughRolling_zip DoughRolling
 cd DoughRolling_zip
 for i in S07 S08 S09 S11 S12 S14 S15 S16 S17 S18 S19 S20 S22 S25 S28 S29 S30 S31 S32 S33 S34 S35 S36 S37 S40 S41 S47 S48 S49 S50 S51 S52 S53 S54 S55
 do
-	# wget http://kitchen.cs.cmu.edu/Main/${i}_Pizza_Video.zip
-	sudo unzip ${i}_Pizza_Video.zip -d ${i}_Pizza_Video
+	wget http://kitchen.cs.cmu.edu/Main/${i}_Pizza_Video.zip
+	unzip ${i}_Pizza_Video.zip -d ${i}_Pizza_Video
 	# mv ${i}_Pizza_Video/*_Pizza_7150991* . && rm -rf ${i}_Pizza_Video
 done
 files=`cat ../../EPIC-Skills2018/dough_rolling_segments.csv | awk -F "," '{ print $1 }' | xargs | sed "s/^[^ ]* //"`
@@ -39,9 +39,9 @@ cd ../
 
 # [Suturing, NeedlePassing and KnotTying]
 # Follow and instructions and download dataset from https://cirl.lcsr.jhu.edu/research/hmm/datasets/jigsaws_release/
-for file in  Suturing 
+for file in Needle_Passing Knot_Tying Suturing
 do
-sudo unzip ${file}.zip && \
+unzip ${file}.zip && \
 	cd ${file} && \
 	rm -r !("video") && \  # Remove all files and folders except `video` directory. Do not run this as a sudo user.
 	mv video/*_capture2.avi . && rm -rf video && cd ../  # Move all video files from video directory
--- prepare_data.py
+++ prepare_data.py
@@ -1,17 +1,20 @@
 
+import csv
 import cv2
 import numpy as np
 import os
 
+from argparse import ArgumentParser
 from concurrent.futures import ProcessPoolExecutor
 from functools import partial
 from tqdm import tqdm
 
 
+DATASET = "/proj/xsjhdstaff4/akorra/datasets/videos"
 optical_flow = cv2.optflow.DualTVL1OpticalFlow_create()
 
 
-def extract_frames(sequence_dirs, save_path, resize=None):
+def extract_frames(sequence_dirs, save_path, resize=None, num_processes=1):
     output_by_input = []
     for sequence_dir in sequence_dirs:
         for video in os.listdir(sequence_dir):
@@ -20,7 +23,7 @@ def extract_frames(sequence_dirs, save_path, resize=None):
             video_path = os.path.join(sequence_dir, video)
             output_by_input.append((video_path, output_dir))
     func = partial(process, resize)
-    with ProcessPoolExecutor() as executor:
+    with ProcessPoolExecutor(max_workers=num_processes) as executor:
         results = list(tqdm(executor.map(func, output_by_input),
                             total=len(output_by_input)))
 
@@ -28,31 +31,53 @@ def extract_frames(sequence_dirs, save_path, resize=None):
 def process(resize, output_by_input):
     input_video_file, output_dir = output_by_input
     os.makedirs(output_dir, exist_ok=True)
+
     cap = cv2.VideoCapture(input_video_file)
     length_video = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    frame_idx = 1
-    prev = None
-    while True:
-        ret, rgb_frame = cap.read()
-        if not ret:
+
+    start_frame = 0
+    end_frame = length_video
+
+    # Only useful for DoughRolling dataset
+    frames_to_consider = {}
+    with open('EPIC-Skills2018/dough_rolling_segments.csv', 'r') as file:
+        frames_to_consider = {row[0]: (row[1], row[2])
+                              for row in csv.reader(file)}
+    vfile_basename = os.path.basename(input_video_file)
+    for key, value in frames_to_consider.items():
+        if key in vfile_basename:
+            start_frame, end_frame = value
             break
-        rgb_file = os.path.join(output_dir, "rgb_{:04d}.npy".format(frame_idx))
-        flow_file = os.path.join(output_dir, "flow_{:04d}.npy".format(frame_idx))
+
+    prev = None
+    for frame_idx in range(end_frame):
+        ret, rgb_frame = cap.read()
+        if (frame_idx < start_frame):
+            continue
+        rgb_file = os.path.join(output_dir, "rgb_{:04d}.npy".format(frame_idx+1))
+        flow_file = os.path.join(output_dir, "flow_{:04d}.npy".format(frame_idx+1))
         if resize:
-            rgb_frame = cv2.resize(rgb_frame, resize)
+            rgb_frame = cv2.resize(rgb_frame, (resize, resize))
         np.save(rgb_file, rgb_frame)
         gray = cv2.cvtColor(rgb_frame, cv2.COLOR_BGR2GRAY)
-        if frame_idx > 1:
+        if prev is not None:
             flow = optical_flow.calc(prev, gray, None)
             np.save(flow_file, flow)
         prev = gray
-        frame_idx += 1
+    cap.release()
 
 
 if __name__ == "__main__":
-    dataset = "dataset"
-    sequences = ['ChopstickUsing', "DoughRolling", "HandDrawing",
-                 "Knot_Tying", "Needle_Passing", "SonicDrawing", "Suturing"]
-    seq_dirs = [os.path.join(dataset, seq) for seq in sequences]
-    output_dir = "frames"
-    extract_frames(seq_dirs, output_dir)
+    parser = ArgumentParser()
+    parser.add_argument('-d', "--dataset-folder", default=DATASET)
+    parser.add_argument('-s', '--sequences', required=True, nargs='+')
+    parser.add_argument('-n', '--num_processes', default=5, type=int)
+    parser.add_argument('-r', '--resize', default=256, type=int)
+    parser.add_argument('-o', '--output-folder', default="frames")
+    args = parser.parse_args()
+    # sequences = ["DoughRolling", "ChopstickUsing", "HandDrawing",
+    #              "SonicDrawing", "Knot_Tying", "Needle_Passing", "Suturing"]
+    seq_dirs = [os.path.join(args.dataset_folder, seq) for seq in args.sequences]
+    extract_frames(seq_dirs, args.output_folder, resize=args.resize,
+                   num_processes=args.num_processes)
+
